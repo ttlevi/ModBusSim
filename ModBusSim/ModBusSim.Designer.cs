1a72ff9d51d602271f7c0b161c0c6f728c2093f3
ttlevi <levente.tahitoth@gmail.com>
2023-10-30 12:04:24 +0100

    read coils and holding registers

--- ModBusSim/ModBusSim.Designer.cs
+++ ModBusSim/ModBusSim.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // ModBusSim
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1570, 550);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1653, 799);
+            this.Controls.Add(this.label1);
             this.Name = "ModBusSim";
             this.Text = "ModBusSim";
+            this.Load += new System.EventHandler(this.ModBusSim_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ModBusSim/ModBusSim.cs
+++ ModBusSim/ModBusSim.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyModbus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,41 @@ namespace ModBusSim
         public ModBusSim()
         {
             InitializeComponent();
+
+            ModbusServer server = new ModbusServer();
+            server.UDPFlag = false;
+            server.Listen();
+
+            ModbusClient client = new ModbusClient();
+            client.IPAddress = "127.0.0.1";
+            client.Connect();
+
+            server.HoldingRegistersChanged += (s, e) =>
+            {
+                Console.WriteLine($"A megváltozott holding regiszter száma: {e}");
+                Console.WriteLine($"Értéke: {server.holdingRegisters[e]}");
+            };
+
+            server.CoilsChanged += (s, e) =>
+            {
+                Console.WriteLine($"A meváltozott coil regiszter száma: {e}");
+                Console.WriteLine($"Értéke: {server.coils[e]}");
+
+            };
+
+            server.LogDataChanged += () =>
+            {
+            };
+
+            server.NumberOfConnectedClientsChanged += () =>
+            {
+                //label1.Text= server.NumberOfConnections.ToString();
+            };
+        }
+
+        private void ModBusSim_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
