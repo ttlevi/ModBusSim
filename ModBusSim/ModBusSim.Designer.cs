83e7d0e4360c8b59393539bf3a1e6ae6e4e679a4
leven <leven@TTLEVI-THINKPAD>
2023-10-30 11:00:55 +0100

    Add project files.

---
+++ ModBusSim/ModBusSim.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ModBusSim
+{
+    partial class ModBusSim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ModBusSim
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1570, 550);
+            this.Name = "ModBusSim";
+            this.Text = "ModBusSim";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
